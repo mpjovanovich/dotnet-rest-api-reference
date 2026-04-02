e478126fce5d54fe0ef575c09e5e9b4b2c157ec1
Mike Jovanovich <mpjovanovich@gmail.com>
2026-04-02 13:23:51 -0400

    Updated birds endpoints and handlers to be split layers

--- Endpoints/BirdsEndpoint.cs
+++ Endpoints/BirdsEndpoint.cs
@@ -1,53 +1,12 @@
-using Microsoft.AspNetCore.Http.HttpResults;
-using DotnetRestApiReference.Models;
-using DotnetRestApiReference.DTOs;
+using DotnetRestApiReference.Handlers;
 
 namespace DotnetRestApiReference.Endpoints;
 
 internal static class BirdsEndpoint
 {
-    private static List<Bird> _birds = new();
-    private static int _nextId = 1;
-
-    static BirdsEndpoint()
-    {
-        _birds.Add(new Bird(_nextId++, "Eastern Bluebird", "Sialia sialis", new List<int> { 1 }));
-    }
-
-    /* ************************************************************
-    // ROUTES
-    * ************************************************************/
     public static void MapRoutes(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/birds", GetBirds);
-        app.MapPost("/birds", CreateBird);
-    }
-
-    /* ************************************************************
-    // HELPERS
-    * ************************************************************/
-    private static BirdResponse ToResponse(Bird b) =>
-        new (b.Id, b.CommonName, b.Species, b.RegionIds);
-
-    /* ************************************************************
-    // HANDLERS
-    * ************************************************************/
-
-    private static Ok<List<BirdResponse>> GetBirds() =>
-        TypedResults.Ok(_birds.Select(ToResponse).ToList());
-
-    // private static Results<Created<BirdResponse>, BadRequest> CreateBird(CreateBirdRequest request)
-    private static Created<BirdResponse> CreateBird(CreateBirdRequest request)
-    {
-        Console.WriteLine($"Creating bird: {request}");
-
-        var bird = new Bird(
-            _nextId++,
-            request.CommonName,
-            request.Species,
-            request.RegionIds ?? new()
-        );
-        _birds.Add(bird);
-        return TypedResults.Created($"/birds/{bird.Id}", ToResponse(bird));
+        app.MapGet("/birds", BirdsHandler.GetBirds);
+        app.MapPost("/birds", BirdsHandler.CreateBird);
     }
 }
---
+++ Handlers/BirdsHandler.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using DotnetRestApiReference.Models;
+using DotnetRestApiReference.DTOs;
+
+namespace DotnetRestApiReference.Handlers;
+
+internal static class BirdsHandler
+{
+    private static List<Bird> _birds = new();
+    private static int _nextId = 1;
+
+    static BirdsHandler()
+    {
+        _birds.Add(new Bird(_nextId++, "Eastern Bluebird", "Sialia sialis", new List<int> { 1 }));
+    }
+
+    /* ************************************************************
+    // ROUTES
+    * ************************************************************/
+    public static void MapRoutes(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/birds", GetBirds);
+        app.MapPost("/birds", CreateBird);
+    }
+
+    /* ************************************************************
+    // HELPERS
+    * ************************************************************/
+    private static BirdResponse ToResponse(Bird b) =>
+        new (b.Id, b.CommonName, b.Species, b.RegionIds);
+
+    /* ************************************************************
+    // HANDLERS
+    * ************************************************************/
+    internal static Ok<List<BirdResponse>> GetBirds() =>
+        TypedResults.Ok(_birds.Select(ToResponse).ToList());
+
+    internal static Created<BirdResponse> CreateBird(CreateBirdRequest request)
+    {
+        Console.WriteLine($"Creating bird: {request}");
+
+        var bird = new Bird(
+            _nextId++,
+            request.CommonName,
+            request.Species,
+            request.RegionIds ?? new()
+        );
+        _birds.Add(bird);
+        return TypedResults.Created($"/birds/{bird.Id}", ToResponse(bird));
+    }
+}
