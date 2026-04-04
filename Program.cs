699d426660af401016776710fb5ef4d95b15339c
Mike Jovanovich <mpjovanovich@gmail.com>
2026-04-04 16:18:43 -0400

    comments

--- Program.cs
+++ Program.cs
@@ -3,8 +3,7 @@ using DotnetRestApiReference.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Tells ASP.NET Core to use RFC 7807 Problem Details for errors when appropriate.
-// Gives us some very basic error handling; all that's needed for this project scope.
+// Gives us basic error handling; all that's needed for this project scope.
 builder.Services.AddProblemDetails();
 
 var app = builder.Build();
